5d1e0d9ebc55e645366a1973efe769897afa4b98
Kiran Thube <kiranthube3137@gmail.com>
2026-06-22 23:39:35 +0530

    Added Header sticky  and the datatypes is  added

--- src/app/features/services/product.service.ts
+++ src/app/features/services/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product } from '../models/product.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,9 @@ export class ProductService {
 
   private http = inject(HttpClient);
 
-  getProducts() {
-    return this.http.get<Product[]>('assets/json-file/products.json');
-  }
-
+  public getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(
+      'assets/json-file/products.json'
+    );
+    }
 }
